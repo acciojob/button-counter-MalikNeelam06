57e38677904c991da37c18ce0c772aa119d655c4
NeelamMalik <nmalik060191@gmail.com>
2023-12-13 17:21:19 +0530

    button

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,14 @@ import './../styles/App.css';
 
 const App = () => {
 let [count, setCount]= useState(0);
-function implement(){
+function implementt(){
   setCount(count+1);
 }
   return (
     <div>
         {/* Do not remove the main div */}
         <p>Button clicked {count} times</p>
-        <button onclick={implement}>Click me</button>
+        <button onclick={implementt}>Click me</button>
     </div>
   )
 }
